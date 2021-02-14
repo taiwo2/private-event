4bae0563a42d1c8db8a47fd7ec1b728c4f2b5363
taiwo2 <adetonat@ymail.com>
2021-02-14 12:31:26 +0100

    Fix linters

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -35,12 +35,12 @@ class EventsController < ApplicationController
   private
 
   def attended_events
-    unless @user_signed_in
+    if @user_signed_in
       @user = User.find_by(id: session[:user_id])
-      if @user
-        @attended_events = Invitation.where(user_id: @user.id).pluck(:event_id)
-      end
-    
+
+    elsif @user
+      @attended_events = Invitation.where(user_id: @user.id).pluck(:event_id)
+    end
   end
 
   def event_params
@@ -49,11 +49,11 @@ class EventsController < ApplicationController
 
   def check_user_signed_in
     super
-   end
+  end
 
   def require_signed_in
-    unless !@user_signed_in
+    if !@user_signed_in
       respond_to { |format| format.html { redirect_to '/sessions/new', alert: 'You have to be signed in!' } }
-    
+    end
   end
 end
